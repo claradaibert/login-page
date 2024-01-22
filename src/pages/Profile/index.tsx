334dde5a1a3053e55cea675565038c460bf50a50
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-01-22 12:55:43 -0300

    Create new profile page

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Hook import
+import { useAuth } from "../../hooks/useAuth";
+
+// Component import
+import PageContainer from "../../components/PageContainer";
+import { Button } from "../../components/Button";
+
+const Profile: React.FC = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    auth.setUser({});
+    auth.setToken("");
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (!auth.token) {
+      navigate("/");
+    }
+  }, []);
+
+  return (
+    <PageContainer>
+      <p className="pageTitle"> Olá, {auth?.user?.name}</p>
+      <Button text={"SAIR"} handleClick={() => logOut()} />
+    </PageContainer>
+  );
+};
+
+export { Profile };
