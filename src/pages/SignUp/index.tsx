9c431b2c114f72778e8cf67df42cece13ae8d08a
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-01-22 13:24:05 -0300

    Adjust signup page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { IoChevronBack } from "react-icons/io5";
+
+// Hook import
+import { useAuth } from "../../hooks/useAuth";
 
 // Component import
 import PageContainer from "../../components/PageContainer";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+
+// service import
+import { api } from "../../services/api";
 
 import * as Style from "./styles";
 
@@ -16,6 +24,9 @@ interface IFormData {
 }
 
 const SignUp: React.FC = () => {
+  const navigate = useNavigate();
+  const auth = useAuth();
+
   const [formData, setFormData] = useState<IFormData>({
     name: "",
     email: "",
@@ -23,52 +34,71 @@ const SignUp: React.FC = () => {
   });
 
   const handleButton = async () => {
-    const schema = yup.object().shape({
-      name: yup
-        .string()
-        .strict()
-        .required("Por favor informe o seu nome")
-        .typeError("O nome deve ser composto de caracteres alfanuméricos"),
-      email: yup
-        .string()
-        .strict()
-        .typeError("O e-mail deve ser composto por caracteres alfanuméricos")
-        .email("O e-mail inserido é inválido")
-        .required("Por favor informe seu e-mail"),
-      password: yup.string().strict().required("Por favor informe a sua senha"),
-    });
-
-    await schema.validate(formData, { abortEarly: false }).catch((err) => {
-      err.inner.forEach((error: any) => {
-        toast.error(error.message);
+    try {
+      const schema = yup.object().shape({
+        name: yup
+          .string()
+          .strict()
+          .required("Por favor informe o seu nome")
+          .typeError("O nome deve ser composto de caracteres alfanuméricos"),
+        email: yup
+          .string()
+          .strict()
+          .typeError("O e-mail deve ser composto por caracteres alfanuméricos")
+          .email("O e-mail inserido é inválido")
+          .required("Por favor informe seu e-mail"),
+        password: yup.string().strict().required("Por favor informe a sua senha"),
       });
-      throw err;
-    });
+  
+      await schema.validate(formData, { abortEarly: false }).catch((err) => {
+        err.inner.forEach((error: any) => {
+          toast.error(error.message);
+        });
+        // eslint-disable-next-line
+        throw 'failed validation';
+      });
 
-    toast.success('Cadastro realizado!');
+      const response = await api.post("signUp", formData)
+
+      auth.setUser(response.data.user);
+      auth.setToken(response.data.token);
+  
+      toast.success("Cadastro realizado!");
+
+      navigate('/profile')
+    } catch(err: any) {
+      if (err === 'failed validation') return;
+      toast.error(err.response.data.message);
+    }
   };
 
   return (
     <PageContainer>
+      <Style.BackButtonContainer onClick={() => navigate("/")}>
+        <IoChevronBack />
+        <p>Voltar</p>
+      </Style.BackButtonContainer>
       <p className="pageTitle"> Faça seu cadastro</p>
-      <Input
-        name="name"
-        value={formData.name}
-        label="Nome"
-        handleChange={(value) => setFormData({ ...formData, name: value })}
-      />
-      <Input
-        name="email"
-        value={formData.email}
-        label="E-mail"
-        handleChange={(value) => setFormData({ ...formData, email: value })}
-      />
-      <Input
-        name="password"
-        value={formData.password}
-        label="Senha"
-        handleChange={(value) => setFormData({ ...formData, password: value })}
-      />
+      <div className="infoGroup">
+        <Input
+          name="name"
+          value={formData.name}
+          label="Nome"
+          handleChange={(value) => setFormData({ ...formData, name: value })}
+        />
+        <Input
+          name="email"
+          value={formData.email}
+          label="E-mail"
+          handleChange={(value) => setFormData({ ...formData, email: value })}
+        />
+        <Input
+          name="password"
+          value={formData.password}
+          label="Senha"
+          handleChange={(value) => setFormData({ ...formData, password: value })}
+        />
+      </div>
       <Button text="CADASTRAR" handleClick={() => handleButton()} />
     </PageContainer>
   );
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,1 +1,18 @@
 import styled from "styled-components";
+
+export const BackButtonContainer = styled.button`
+    display: flex;
+    color: ${({theme}) => theme.background_primary_blue};
+    font-size: 0.875rem;
+    align-items: center;
+    gap: 0.3rem;
+    align-self: left;
+    border: none;
+    position: relative;
+    right: 33%;
+    bottom: 50px;
+
+    @media screen and (max-width: 700px) {
+        bottom: 30px;
+    }
+`
